4ff3a8345eb057b842dc0a7e39cfcf9caf0d0e74
iamnana-tyc <nanatyc7@gmail.com>
2025-03-22 12:00:04 +0500

    Implement employee mapper

--- SwiftHRBack/src/main/java/com/iamnana/swift_HR_Backend/mapper/EmployeeMapper.java
+++ SwiftHRBack/src/main/java/com/iamnana/swift_HR_Backend/mapper/EmployeeMapper.java
@@ -1,4 +1,29 @@
 package com.iamnana.swift_HR_Backend.mapper;
 
+import com.iamnana.swift_HR_Backend.dto.EmployeeDto;
+import com.iamnana.swift_HR_Backend.dto.EmployeeResponse;
+import com.iamnana.swift_HR_Backend.model.Employee;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeMapper {
+    public Employee mapToEmployee(EmployeeDto employeeDto){
+
+        Employee employee = new Employee();
+        employee.setFirstname(employeeDto.firstname());
+        employee.setLastname(employeeDto.lastname());
+        employee.setWorkEmail(employeeDto.workEmail());
+        employee.setDesignation(employeeDto.designation());
+        return employee;
+    }
+
+    public EmployeeResponse mapToEmployeeResponse(Employee employee){
+        return new EmployeeResponse(
+                employee.getId(),
+                employee.getFirstname(),
+                employee.getLastname(),
+                employee.getWorkEmail(),
+                employee.getDesignation()
+        );
+    }
 }
