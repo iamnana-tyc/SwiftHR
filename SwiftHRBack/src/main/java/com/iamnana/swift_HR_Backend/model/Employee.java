4b44df51efbf0b04f6bdb37c977330716592a663
iamnana-tyc <nanatyc7@gmail.com>
2025-03-22 12:00:27 +0500

    Add employee object

--- SwiftHRBack/src/main/java/com/iamnana/swift_HR_Backend/model/Employee.java
+++ SwiftHRBack/src/main/java/com/iamnana/swift_HR_Backend/model/Employee.java
@@ -1,4 +1,21 @@
 package com.iamnana.swift_HR_Backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String workEmail;
+    private String designation;
 }
