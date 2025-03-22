8fb4dea480fa00ba73fa0c4e7dab74ff72e7fb04
iamnana-tyc <nanatyc7@gmail.com>
2025-03-22 13:29:21 +0500

    First commit

---
+++ SwiftHRBack/src/main/java/com/iamnana/swift_HR_Backend/SwiftHrBackApplication.java
@@ -0,0 +1,13 @@
+package com.iamnana.swift_HR_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftHrBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftHrBackApplication.class, args);
+	}
+
+}
---
+++ SwiftHRBack/src/test/java/com/iamnana/swift_HR_Backend/SwiftHrBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamnana.swift_HR_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftHrBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
